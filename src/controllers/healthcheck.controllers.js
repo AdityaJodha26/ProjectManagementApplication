74c058936a99579abb435e8b20dc1bb63dce5897
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-07-13 18:03:49 +0000

    commit

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,10 +1,17 @@
 import {ApiResponse} from "../utils/apiResponse.js" ; 
 
-const healthcheck = (req , res) =>{
+/*
+const healthcheck = (req , res , next ) =>{
     try{
         res.status(200).json(new ApiResponse(200 , {message:"Server is running"}))
     }catch(error){
+        next(error) ; 
 
     }
 }
+    */
+import { asyncHandler } from "../utils/async-handler.js";
+const healthcheck = asyncHandler( async(req , res)=>{
+    res.status(200).json( new ApiResponse(200 , {message : "Server is still running "}))
+})
 export {healthcheck} ; 
---
+++ src/models/users.models.js
@@ -0,0 +1,107 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import crypto from "crypto" ; 
+const userSchema = new Schema(
+    { avatar: {
+        type:{
+            url: String , 
+            localPath: String ,
+        },
+        default:{
+            url:`https://www.istockphoto.com/illustrations/default-user-icon`,
+            localPath: String , 
+        }
+
+    },
+    user:{
+        type: String , 
+        required: true , 
+        unique:true , 
+        lowercase: true , 
+        trim: true ,
+        index: true , 
+    },
+    email:{
+        type: String , 
+        required: true , 
+        unique:true , 
+        lowercase: true , 
+        trim: true ,
+        index: true , 
+    },
+    fullname: {
+        type: String , 
+        trim: true ,
+    },
+    password: {
+        type: String , 
+        required: [true , "Password is required"] ,
+
+    },
+    isEmailVerified : {
+        type: boolean ,
+        default: false , 
+
+    },
+    refreshToken:{
+        type:String 
+    },
+    forgetPasswordToken:{
+        type:String , 
+    },
+    forgetPasswordExpiry:{
+        type: Date , 
+    },
+    emailVerificationToken:{
+        type:String , 
+
+    }, 
+    emailVerificationExpiry:{
+        type:Date , 
+    }
+
+    },
+    {
+        timestamps: true ,
+    })
+
+userSchema.pre("save" , async function(next) {
+    if(!this.isModified("password")) return next() ;
+    this.password = await bcrypt.hash(this.password , 10) ; 
+    next() ; 
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password) ; 
+    
+};
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id ,
+            email:this.email ,
+            username: this.username 
+        },
+        process.env.ACCESS_TOKEN_SECRET , 
+        {expiry: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        { _id : this._id , 
+        } ,
+        process.env.REFRESH_TOKEN_SECRET , 
+        {expiryIn: process.env.REFRESH_TOKEN_EXPIRY}
+        
+        
+)
+}
+
+userSchema.methods.generateTemporaryToken = function(){
+    const unHashedToken = crypto.randomBytes(20).toString("hex") ;
+    const HashedToken = crypto.hash("sha256").update(unHashedToken).digest("hex"); 
+    const tokenExpiry = Date.now() + (20*60*1000) ; 
+    return {unHashedToken , HashedToken , tokenExpiry} ; 
+}
+
+export const User = mongoose.model("User" ,userSchema) ;
