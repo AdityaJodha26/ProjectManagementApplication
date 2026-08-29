af6e3ca5a6f2b537e1ac85cef4c7f54ae3de5ae7
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-08-29 04:21:40 +0000

    commit

--- src/validators/index.js
+++ src/validators/index.js
@@ -45,4 +45,31 @@ const userLoginValidator = ()=>{
         
     ]
 }
+
+const userChangeCurrentPasswordValidator = () =>{
+    return []
+        body("oldPassword").notEmpty().withMessage("Old Password is required") 
+        ,
+        body("newPassword").notEmpty().withMessage("New Password is required")
+
+
+}
+
+const userForgotPasswordValidator = ()=>{
+    return[
+        body("email")
+            .notEmpty()
+            .withMessage("Email is Required")
+            .isEmail()
+            .withMessage("Email is invalid")
+    ]
+}
+
+const userResetForgotPasswordValidator = ()=>{
+    return [
+        body("newPassword")
+            .notEmpty()
+            .withMessage("Password is required")
+    ]
+}
 export {userRegisterValidator, userLoginValidator} ; 
