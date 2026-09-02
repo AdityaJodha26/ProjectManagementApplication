dd08ab5a559e6cece84261e25fd916185b789776
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-09-02 16:36:51 +0000

    commit

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -7,6 +7,78 @@ import {ApiResponse} from "../utils/apiResponse.js"
 import mongoose from "mongoose" 
 import { UserRolesEnum } from "../utils/constants.js"
 
+const getProject = asyncHandler(async(req ,res)=>{
+
+    const project = await ProjectMember.aggregate([
+        {
+            $match:{
+                user: new mongoose.Types.ObjectId(req.user.id)
+
+            }
+        }
+        ,
+        {
+            $lookup:{
+                from:"project",
+                localfield:"project",
+                foreignfield:"id" , 
+                as:"projects",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"projectmembers" , 
+                            localfield:"id",
+                            foreignfield:projects ,
+                            as:"projectmembers"
+                        }
+                    },
+                    {
+                        $addfields:{
+                            members:{
+                                $size:"$projectMembers"}
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$project"
+        }
+        ,{
+            $project:{
+                project:{
+                    id:1 , 
+                    name:1 , 
+                    description:1 , 
+                    member: 1 , 
+                    createdBy:1 , 
+                    createdAt: 1 , 
+                }
+                , 
+                role:1 , 
+                id:0
+            }
+        }
+    ])
+
+    return res
+            .status(200) 
+            .json(new ApiResponse(200 , project , "projects fetched successfully"))
+})
+
+const getProjectById = asyncHandler(async (req, res)=>{
+    
+    const {projectId} = req.params 
+    const project = await Project.findById(
+        projectId
+    )
+    if(!project) {
+        throw new ApiErrors(400 ,"Project not found")
+    }
+    
+    return res  
+            .status(200 , new ApiResponse(200 , project , "Project founded successfully"))
+})
 const create = asyncHandler(async (req ,res)=>{
     const { name , description } = req.body 
 
