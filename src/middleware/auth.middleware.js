f5e6ff15807648564ff2c72925ab21f54aa67fcb
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-09-03 09:41:28 +0000

    validating project permission

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,6 +2,7 @@ import { User } from "../models/users.models.js";
 import { ApiErrors } from "../utils/apiErrors.js" ; 
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt from "jsonwebtoken";
+import { AvailableUserRole } from "../utils/constants.js";
 
 export const verifyJWT = asyncHandler(async(req, res ,next)=>{
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer " , "Unauthorized request")
@@ -24,3 +25,33 @@ export const verifyJWT = asyncHandler(async(req, res ,next)=>{
 
     }
 })
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler( async (req ,res)=>{
+
+        const {projectId} = req.params 
+        if(!projectId){
+            throw new ApiError(403 , "Project with this projectId doesnt exist")
+        }
+
+        const projectMember = await ProjectMember.findById({
+            project: new mongoose.Types.ObjectId(projectId) ,
+            user:  new mongoose.Types.ObjectId(userId)
+        })
+        if(!projectMember) {
+            throw new ApiError(403 , "project doesnt exist")
+        }
+
+        const givenrole = projectMember?.roles
+        req.user.role = givenrole
+
+        if(!roles.includes(givenrole)){
+            throw new ApiErrors(402 , "given role doesnt exist")
+        }
+
+        next()
+
+
+    
+    })
+}
